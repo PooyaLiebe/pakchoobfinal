e8be632a304234eeaacb99a65566cf0d769ef9a1
PooyaLiebe <pooya.payvar26@gmail.com>
2025-03-23 09:33:26 +0330

    Add files via upload

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Route, Routes, useLocation } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Navigate, Route, Routes, useLocation } from "react-router-dom";
 import OperatorLogin from "./pages/OperatorLogin";
 import TechnicianLogin from "./pages/TechnicianLogin";
 import AdminLogin from "./pages/AdminLogin";
@@ -9,40 +9,87 @@ import AdminDashboard from "./pages/AdminDashboard";
 import OperatorDashboard from "./pages/OperatorDashboard";
 import TechnicianDashboard from "./pages/TechnicianDashboard";
 import Start from "./pages/StartPage";
-import { Sidebar } from "lucide-react";
+import Sidebar from "./components/Sidebar";
+
+function Logout() {
+  localStorage.clear();
+  return <Navigate to={"/login"} />;
+}
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [loading, setLoading] = useState(true);
   const location = useLocation();
-  const showSidebar = ![
-    "/",
-    "/login",
-    "/register",
-    "/adminlogin",
-    "/operatorlogin",
-    "/technicianlogin",
-  ].includes(location.pathname);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      setIsLoggedIn(true);
+    }
+    setLoading(false);
+  }, []);
+
+  const handleLoginSuccess = () => {
+    setIsLoggedIn(true);
+  };
+
+  const showSidebar =
+    isLoggedIn &&
+    ![
+      "/",
+      "/login",
+      "/register",
+      "/adminlogin",
+      "/operatorlogin",
+      "/technicianlogin",
+    ].includes(location.pathname);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
 
   return (
-    <div className="flex h-screen bg-gray-900 text-gray-100 overflow-hidden">
-      {/* BackGround */}
-      <div className="fixed inset-0 z-0">
-        <div className="absolute inset-0 bg-gradient-to-br from-gray-900 via-gray-800 to-gray-900 opacity-80" />
-        <div className="absolute inset-0 backdrop-blur-sm" />
+    <div>
+      <div className="flex h-screen bg-gray-900 text-gray-100 overflow-hidden">
+        {/* BackGround */}
+        <div className="fixed inset-0 z-0">
+          <div className="absolute inset-0 bg-gradient-to-br from-gray-900 via-gray-800 to-gray-900 opacity-80" />
+          <div className="absolute inset-0 backdrop-blur-sm" />
+        </div>
+
+        {showSidebar && <Sidebar />}
+
+        <Routes>
+          <Route path="/" element={<Start />} />
+          <Route
+            path="/login"
+            element={<LoginMainForm onLoginSuccess={handleLoginSuccess} />}
+          />
+          <Route path="/register" element={<Register />} />
+          <Route path="/adminlogin" element={<AdminLogin />} />
+          <Route path="/operatorlogin" element={<OperatorLogin />} />
+          <Route path="/technicianlogin" element={<TechnicianLogin />} />
+          <Route
+            path="/admindashboard"
+            element={
+              isLoggedIn ? <AdminDashboard /> : <Navigate to={"/login"} />
+            }
+          />
+          <Route
+            path="/operatordashboard"
+            element={
+              isLoggedIn ? <OperatorDashboard /> : <Navigate to={"/login"} />
+            }
+          />
+          <Route
+            path="/techniciandashboard"
+            element={
+              isLoggedIn ? <TechnicianDashboard /> : <Navigate to={"/login"} />
+            }
+          />
+          <Route path="/logout" element={<Logout />} />
+        </Routes>
       </div>
-
-      {showSidebar && <Sidebar />}
-
-      <Routes>
-        <Route path="/" element={<Start />} />
-        <Route path="/login" element={<LoginMainForm />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/adminlogin" element={<AdminLogin />} />
-        <Route path="/operatorlogin" element={<OperatorLogin />} />
-        <Route path="/technicianlogin" element={<TechnicianLogin />} />
-        <Route path="/admindashboard" element={<AdminDashboard />} />
-        <Route path="/operatordashboard" element={<OperatorDashboard />} />
-        <Route path="/techniciandashboard" element={<TechnicianDashboard />} />
-      </Routes>
     </div>
   );
 }
--- frontend/src/pages/OperatorDashboard.jsx
+++ frontend/src/pages/OperatorDashboard.jsx
@@ -1,7 +1,36 @@
+/* eslint-disable no-unused-vars */
 import React from "react";
+import Header from "../components/Common/Header";
+import StatCard from "../components/Common/StatCard";
+import { motion } from "framer-motion";
+import { Zap } from "lucide-react";
 
 const OperatorDashboard = () => {
-  return <div>OperatorDashboard</div>;
+  return (
+    <div className="flex-1 overflow-auto relative z-10">
+      <Header title="Dashboard" />
+      <main className="max-w-7xl mx-auto py-6 px-4 lg:px-8">
+        {/* STATS */}
+        <motion.div
+          className="grid grid-cols-2 gap-5 sm:grid-cols-2 lg:grid-cols-4 mb-8 "
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1 }}
+        >
+          <StatCard name="PM" icon={Zap} value={"21"} color="#6366f1" />
+          <StatCard name="EM" icon={Zap} value={"60"} color="#8B5CF6" />
+          <StatCard name="CM" icon={Zap} value={"52"} color="#ec4899" />
+          <StatCard name="GM" icon={Zap} value={"30"} color="#10b981" />
+        </motion.div>
+        {/* CHARTS */}
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
+          {/* <EmOverviewChart />
+          <FormOverviewPieChart />
+          <FormOverviewBarChart /> */}
+        </div>
+      </main>
+    </div>
+  );
 };
 
 export default OperatorDashboard;
